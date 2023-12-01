511de1513dec4e9b272682f709cb8764943476e5
lakshmanan2004 <124753145+lakshmanan2004@users.noreply.github.com>
2023-12-01 20:31:01 +0530

    Add files via upload

---
+++ Experiment - 1.r
@@ -0,0 +1,13 @@
+cat("Enter your name: ")
+name <- readline()
+
+# Ask for user's age
+cat("Enter your age: ")
+age <- readline()
+
+# Display the values
+cat("Your name is ", name, " and your age is ", age, "\n")
+
+# Print the version of R installation
+cat("The version of R installation is: \n")
+print(version)
